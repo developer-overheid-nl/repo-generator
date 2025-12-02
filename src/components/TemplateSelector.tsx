575164d05cbe3bd4d2f491fe71afa86a22bdbdd8
Tom Ootes <tom@ootes.io>
2025-12-02 10:51:19 +0100

    feat: fetch template when new temp selected

--- src/components/TemplateSelector.tsx
+++ src/components/TemplateSelector.tsx
@@ -1,19 +1,34 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 interface Props {
   className?: string;
+  setGitTemplate: (template: string | null) => void;
 }
 
-const TemplateSelector: FC<Props> = ({ className }) => {
+export const templateRoutes = ["SECURITY.md","CODE_OF_CONDUCT.md","LICENCE.md","publiccode.yml","README.md"];
+
+const TemplateSelector: FC<Props> = ({ className, setGitTemplate }) => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const templates = ["SECURITY.md","CODE_OF_CONDUCT.md","LICENCE.md","publiccode.yml","README.md"];
+    useEffect(() => {
+
+      const url = `https://raw.githubusercontent.com/developer-overheid-nl/repository-template/refs/heads/main/templates${location.pathname}`;
+
+      fetch(url)
+        .then(response => response.text())
+        .then(text => {
+          setGitTemplate(text)
+        })
+        .catch(error => {
+          console.error('Failed to fetch template:', error)
+        });
+    }, [location.pathname, setGitTemplate]);
 
   return (
     <select value={location.pathname} onChange={event => navigate(event.target.value)} className={className}>
-      {templates.map(templateName => (
+      {templateRoutes.map(templateName => (
         <option key={templateName} value={`/${templateName}`}>
           {templateName}
         </option>
